03b436421583d656427ea83b6a0497972ae98cb8
Filip Kajzer <fkjzr@Filips-Air.fritz.box>
2022-06-19 19:26:19 +0200

    feat(Android): add android paths to fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -43,6 +43,18 @@ function __check_rvm --on-variable PWD --description 'Do nvm stuff'
   end
 end
 
+# Android 
+set --export ANDROID $HOME/Library/Android;
+set --export ANDROID_HOME $ANDROID/sdk;
+set -gx PATH $ANDROID_HOME/tools $PATH;
+set -gx PATH $ANDROID_HOME/tools/bin $PATH;
+set -gx PATH $ANDROID_HOME/platform-tools $PATH;
+set -gx PATH $ANDROID_HOME/emulator $PATH
+
+set --export JAVA_HOME /Applications/Android\ Studio.app/Contents/jre/jdk/Contents/Home;
+set -gx PATH $JAVA_HOME/bin $PATH;
+
+
 switch (uname)
   case Darwin
     source (dirname (status --current-filename))/config-osx.fish
